3aee2f73fd1efde95c6b3a8eaf56dba480650922
Camille Baillat <camille.baillat@protonmail.com>
2017-12-05 19:40:28 +0100

    [memory] changed the ft_realloc function to take in account smaller sizes

--- srcs/memory/ft_realloc.c
+++ srcs/memory/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 20:23:03 by cbaillat          #+#    #+#             */
-/*   Updated: 2017/12/04 20:35:01 by cbaillat         ###   ########.fr       */
+/*   Updated: 2017/12/05 15:50:11 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 /*
 ** This function returns a pointer to a memory zone of the new length.
 ** If a void ptr is passed as a parameter, it behaves like malloc.
+** If we ask for a bigger size, the rest of the memory zone is set to 0.
 */
 
 void	*ft_realloc(void *ptr, size_t len, size_t new_len)
@@ -30,7 +31,10 @@ void	*ft_realloc(void *ptr, size_t len, size_t new_len)
 	if ((tmp = malloc(new_len)) == NULL)
 		return (NULL);
 	if (new_len >= len)
+	{
 		ft_memcpy(tmp, ptr, len);
+		ft_bzero(tmp + len, new_len - len);
+	}
 	else
 		ft_memcpy(tmp, ptr, new_len);
 	free(ptr);
